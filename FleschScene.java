b6334bdca6cbd0efb2250fb2a69dab961d23e7e9
Anusha Saha <44592491+anushasaha16@users.noreply.github.com>
2020-08-29 09:20:33 -0500

    Update FleschScene.java

--- FleschScene.java
+++ FleschScene.java
@@ -2,14 +2,19 @@ import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-
+import javafx.scene.text.TextAlignment;
+/**
+ * Scene that is set when the application is viewed
+ * @author anush
+ *
+ */
 public class FleschScene extends Scene{
 
 	private BorderPane myRoot;
@@ -34,38 +39,57 @@ public class FleschScene extends Scene{
 		input.setWrapText(true);
 		input.setPrefSize(450, 450);
 	
-		VBox contents = new VBox();
-		HBox h = new HBox();
-		h.getChildren().add(input);
-		Button fleschIndex = new Button("Flesch Index");
-		h.getChildren().add(fleschIndex);
-		h.setSpacing(30);
-		h.setAlignment(Pos.CENTER);
-		contents.getChildren().add(h);
-		
-		TextField displayScore = new TextField();
-		HBox score = new HBox();
-		Text t = new Text("Flesch Index");
+		HBox contents = new HBox();
+		VBox score = new VBox();
+		contents.getChildren().add(input);
+		Button fleschIndex = new Button("Calculate Flesch Index");
 		Button clear = new Button("Clear");
-		score.getChildren().add(displayScore);
+		HBox buttons = new HBox();
+		buttons.getChildren().add(fleschIndex);
+		buttons.getChildren().add(clear);
+		buttons.setSpacing(30);
+		buttons.setAlignment(Pos.CENTER);
+		score.getChildren().add(buttons);
+		contents.getChildren().add(score);
+		
+		Text s = new Text("");
+		s.setTextAlignment(TextAlignment.CENTER);
+		s.setFont(Font.font("Helvetica", 30)); 
+		Text t = new Text("");
+		t.setTextAlignment(TextAlignment.CENTER);
+		s.setFont(Font.font("Helvetica", 20)); 
+		score.getChildren().add(s);
 		score.getChildren().add(t);
-		score.getChildren().add(clear);
 		score.setSpacing(20);
 		
 		score.setAlignment(Pos.CENTER);
-		contents.getChildren().add(score);
 		contents.setSpacing(30);
-		
+		contents.setAlignment(Pos.CENTER);
 		myRoot.setCenter(contents);
 		
-		
+		/**
+		 * Sets the fleschIndex button to display the flesch readability score of whatever text the user inputted and 
+		 * information regarding the respective target audience once the button is pressed. 
+		 * If the user did not input any text, they are prompted to do so.
+		 */
 		fleschIndex.setOnAction(e -> {
 			String i = input.getText();
 			myDocument.setText(i);
 			double d = myDocument.getFleschScore();
-			displayScore.setText(d + "");
+			if(myDocument.getText().isEmpty()) {
+				s.setText("Please enter text");
+			}
+			else {
+			s.setText("Your flesch readability score is " + d);
+			t.setText(myDocument.targetAudience(d));
+			t.setWrappingWidth(400);
+			}
+			
 		});
 		
+		/**
+		 * Sets the clear button to empty the text box once pressed
+		 */
 		clear.setOnAction(e -> {
 			input.setText("");
 		});
