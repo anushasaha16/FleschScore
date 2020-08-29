4f453e6564e73fb80f161f98e29afbd459ecfda0
Anusha Saha <44592491+anushasaha16@users.noreply.github.com>
2020-08-29 09:19:41 -0500

    Update FleschApp.java

--- FleschApp.java
+++ FleschApp.java
@@ -1,6 +1,10 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
-
+/**
+ * Launches Java Application
+ * @author anush
+ *
+ */
 public class FleschApp extends Application{
 
 	private View myView;
